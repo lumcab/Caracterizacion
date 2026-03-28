b028177119dbc567ec3098f78927b4bd532e862d
Luis Miguel Cabrera <lumcab@gmail.com>
2026-03-28 15:18:27 -0500

    JSON, buscar/guardar y mostrar conteo

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const APP_CONFIG = {
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbyZUnnhCw5KiCK1lvPFrTEKkR_JdPNa5Srig6cPA4wccN9uQWuBNBoA_IhY4uYNhBcLag/exec',
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbyQeMva1me7uh69WjqdU0Ac0mF9kZJW5RV4ZPjMGCuVHb8M7IT-t8kI5_u6M91BoFQn5w/exec',
   sheetId: '1ndYw8lA_7qO7o-0-9OT9B7kTOAwKu_Jy3RI7eYTo0w8',
   sheetGid: '0',
-  sectionIds: ['sec-personal', 'sec-academico', 'sec-familiar'],
+  sectionIds: [ 'sec-personal', 'sec-academico', 'sec-familiar' ],
   requiredMessage: 'Completa los campos obligatorios (*) antes de continuar.'
 };
